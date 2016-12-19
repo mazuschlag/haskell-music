b3d59d006b15d0955c6a1a755993d6e7b64d75e9
Mark Zuschlag <markazuschlag@gmail.com>
2016-12-19 02:53:28 +0000

    Added Error value to Token data type

--- Parser/Tokenizer.hs
+++ Parser/Tokenizer.hs
@@ -12,23 +12,24 @@ type Tokens = [Token]
 
 data Token = Token {f :: Form, v :: String, d :: Rational}
 
-data Form = Note | Rest | Chord | Space | Empty | Key | TimeSig
-          | Error deriving (Show, Ord, Eq, Read, Enum, Bounded)
+data Form = Note | Rest | Chord | Error | Space | Empty   
+    deriving (Show, Ord, Eq, Read, Enum, Bounded)
 
 instance Show Token where
     show (Token Note v d) = (show Note) ++ " " ++ v ++ " " ++ (show d) ++ " "
     show (Token Chord v d) = (show Chord) ++ " " ++ v ++ " " ++ (show d) ++ " "
     show (Token Rest v d) = (show Rest) ++ " " ++ v ++ " " ++ (show d) ++ " "
+    show (Token Error v d) = (show Error) ++ " " ++ v ++ " "
     show (Token Space v d) = (show Space) ++ " "
 
 tokenizePhrase :: Phrase -> Tokens
 tokenizePhrase phrase =
     let tN = tokenizeNotes (dropWhile (/= '|') phrase) (Token Empty [] (0%1))
         tSF = tokenizeSharpsFlats phrase
-    in map (finalizeTokens tSF) tN
+    in map (tokenizeFinal tSF) tN
 
-finalizeTokens :: Map.Map Char Char -> Token -> Token
-finalizeTokens tSF (Token f v d) =
+tokenizeFinal :: Map.Map Char Char -> Token -> Token
+tokenizeFinal tSF (Token f v d) =
     Token f (concat . map (addSharpFlat tSF) $ v) d
     where addSharpFlat tSF n = if n `Map.member` tSF then [n]++[tSF!n] else [n]
 
@@ -42,28 +43,26 @@ tokenizeSharpsFlats line =
 -- capitalizes all notes and rests
 tokenizeNotes :: Phrase -> Token -> Tokens
 tokenizeNotes [] (Token f v d) = if f /= Empty then (Token f v d):[] else []
-tokenizeNotes (n:line) (Token f v d) = case checkForm n f d of
-    1 -> appendToken line (Token f v d) (Token Empty [] (0%1))
-    2 -> appendToken line (Token f v d) (Token Space [] (0%1))
-    3 -> appendToken line (Token f v d) (Token Note [(toUpper n)] (0%1))
-    4 -> tokenizeNotes line (Token f v $ calcDur d n)
-    5 -> tokenizeNotes line (Token f (v++(convertSharpFlat[n])) d)
-    6 -> appendToken line (Token Chord v d) (Token Chord [(toUpper n)] (0%1))
-    7 -> tokenizeNotes line (Token Note ((toUpper n):v) d)
-    8 -> tokenizeNotes line (Token Rest ((toUpper n):v) d)
-    0 -> tokenizeNotes line (Token f v d)
+tokenizeNotes (n:line) (Token f v d) = case tokenizeForm n f d of
+    1 -> appendToken line (Token f v d) (Token Space [] (0%1))
+    2 -> appendToken line (Token f v d) (Token Note [n] (0%1))
+    3 -> tokenizeNotes line (Token f v $ calcDur d n)
+    4 -> tokenizeNotes line (Token f (v++(convertSharpFlat[n])) d)
+    5 -> appendToken line (Token Chord v d) (Token Chord [n] (0%1))
+    6 -> tokenizeNotes line (Token Note (n:v) d)
+    7 -> tokenizeNotes line (Token Rest (n:v) d)
+    0 -> tokenizeNotes line (Token Error (v++[n]) d)
     where appendToken l oldT newT = oldT : tokenizeNotes l newT
 
-checkForm :: Char -> Form -> Rational -> Int
-checkForm n f d
-    | (isSpace n) && (f == Space)                  = 1
-    | (isSpace n) && (f >= Note && f <= Chord)     = 2
-    | (isNote n) && (f == Space)                   = 3
-    | (isDur n) && (f >= Note && f <= Chord)       = 4
-    | (isSharpFlat n) && (f == Note || f == Chord) = 5
-    | (isNote n) && (f == Note)                    = 6
-    | (isNote n) && (f == Empty)                   = 7
-    | (isRest n) && (f == Empty)                   = 8
+tokenizeForm :: Char -> Form -> Rational -> Int
+tokenizeForm n f d
+    | (isSpace n)                                  = 1
+    | (isNote n) && (f == Space)                   = 2
+    | (isDur n) && (f >= Note && f <= Chord)       = 3
+    | (isSharpFlat n) && (f == Note || f == Chord) = 4
+    | (isNote n) && (f == Note) && (d > 0%1)       = 5
+    | (isNote n) && (f == Empty)                   = 6
+    | (isRest n) && (f == Empty)                   = 7
     | otherwise                                    = 0
 
 -- Some Helper functions
@@ -72,7 +71,7 @@ calcDur d '.' = d + d * (1%2)
 calcDur d ',' = d + d * (3%4)
 calcDur d n
     | d == (0%1) = d + noteMap ! n
-    | d == (1%8) = d * (1%2)
+    | d == (1%8) = if n == ')' then d * (1%2) else (-1%1)
     | otherwise  = (-1%1)
 
 convertSharpFlat :: String -> String
