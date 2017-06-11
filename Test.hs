ee0cc7575fc70a65ac40768275efc6fdde4eab2f
mazuschlag <markazuschlag@gmail.com>
2017-06-11 21:54:15 +0900

    Removed Test.hs

--- Test.hs
+++
@@ -1,7 +0,0 @@
-module Test where
-
-import Euterpea
-
-mel1 :: Music Pitch
-mel1 = (Prim (Note (1 / 4) (As,5)) :+: ((Prim (Note (1 / 8) (B,4)) :=: (Prim (Note (1 / 8) (C,4)) :=: Prim (Rest (0 / 1)))) :+: (Prim (Rest (1 / 4)) :+: (Prim (Note (1 / 4) (D,3)) :+: Prim (Rest (0 / 1)))))) :=: Prim (Rest (0 /
- 1))
